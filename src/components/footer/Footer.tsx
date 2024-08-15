6e45864a93b0895bee5ceb83202b4c7cf5f505e2
berry <100073317+berryywise@users.noreply.github.com>
2024-08-15 19:43:19 +0200

    Add shared footer layout and placeholders

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -14,10 +14,36 @@ export const FooterBanner = () => {
     )
 }
 
+//TODO Implement flexbox and layout here!
+
 const Footer = () => {
     return (
         <div className='footer-container'>
-
+            <div className='footer-header'>
+                <div className='footer-address'>
+                    <p>401 University</p>
+                </div>
+                <div className='footer-links'>
+                    <p>Links</p>
+                    <p>Home</p>
+                    <p>Shop</p>
+                    <p>Shop</p>
+                    <p>Shop</p>
+                </div>
+                <div className='footer-help-links'>
+                    <p>Help</p>
+                    <p>Payment Options</p>
+                    <p>Payment Options</p>
+                    <p>Payment Options</p>
+                </div>
+                <div className='footer-credits'>
+                    <p>Beatton 2023</p>
+                    <p>Beatton 2023</p>
+                </div>
+            </div>
+            <div className='footer-title-container'>
+                <p className='footer-title'>Beatton</p>
+            </div>
         </div>
     );
 }
