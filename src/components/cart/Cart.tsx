d1a28d2d6150ce758807f62a10509a346bd7288c
100073317+berryywise@users.noreply.github.com <164911049+froggydevie@users.noreply.github.com>
2024-08-19 21:01:51 +0200

    finish cart css design, add mobile responsiveness

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,34 +1,33 @@
 import React from "react";
 
 import "./Cart.css";
-import placeholder from "../../assets/products/console-table.jpg"
+import placeholder from "../../assets/products/console-table.jpg";
 
 const CartItems = () => {
-    return (
-        <div className="cart-items-body">
-            <div className="cart-image-body">
-            <img src={placeholder} width="150px" alt="cart image" />
-            </div>
-            <div className="cart-mid-body">
-                <div className="class-mid-info">
-                <p>LEVIOSA</p>
-                <p>Nomad Snow Chair</p>
-                </div>
-                <div className="class-quantity-info">
-                    <p>Quantity</p>
-                    <button>-</button>
-                    <p>1</p>
-                    <button>+</button>
-                </div>
-                <div className="cart-right-body">
-                    <p>$1.599</p>
-                    <p>Remove</p>
-                </div>
-            </div>
+  return (
+    <div className="cart-items-body">
+      <div className="cart-image-body">
+        <img src={placeholder} width="115px" alt="cart image" />
+      </div>
+      <div className="cart-mid-body">
+        <div className="cart-mid-info">
+          <p>LEVIOSA</p>
+          <p className="cart-subheader-title">Nomad Snow Chair</p>
         </div>
-    );
-}
-
+        <div className="cart-quantity-info">
+          <p className="cart-quantity-title">Quantity</p>
+          <button className="cart-quantity-minus">-</button>
+          <input type="text" className="cart-quantity-input" placeholder="1" />
+          <button className="cart-quantity-plus">+</button>
+        </div>
+      </div>
+      <div className="cart-right-body">
+        <p>$1.599</p>
+        <p className="cart-remove-title">Remove</p>
+      </div>
+    </div>
+  );
+};
 
 const Cart = () => {
   return (
@@ -39,10 +38,11 @@ const Cart = () => {
       </div>
       <div className="cart-body">
         <CartItems />
+        <CartItems />
       </div>
       <div className="cart-footer">
-        <p>Estimated shipping within 10 - 14 working days</p>
-        <button>Checkout ( $ 2.598 )</button>
+        <p className="cart-footer-title">Estimated shipping within 10 - 14 working days</p>
+        <button className="cart-footer-button">Checkout <span className="cart-footer-total">( $ 2.598 )</span></button>
       </div>
     </div>
   );
