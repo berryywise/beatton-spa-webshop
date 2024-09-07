de82234d88e34fe3a4ef2e25db8b4e63b0bf535d
berry <100073317+berryywise@users.noreply.github.com>
2024-09-07 14:21:04 +0200

    Update readme - Cleanup files

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,5 @@
+import './App.css'
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -7,6 +9,7 @@ import ErrorPage from "./components/misc/ErrorPage.tsx";
 import About from "./components/about/About.tsx";
 import Contact from "./components/contact/Contact.tsx";
 import { CartProvider } from "./components/cart/CartProvider.tsx";
+
 
 const router = createBrowserRouter([
   {
