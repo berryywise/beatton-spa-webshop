2cba23db157dcd087cd7ba322b33ab1dbf9f5b3c
berry <100073317+berryywise@users.noreply.github.com>
2024-10-24 21:45:05 +0200

    Fix database connection URL

--- server/config/db.mjs
+++ server/config/db.mjs
@@ -1,15 +1,11 @@
-import 'dotenv/config'
-import pkg from 'pg';
+import "dotenv/config";
+import pkg from "pg";
 const { Pool } = pkg;
 
-export const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
-})
+export const pool = new Pool(
+  {connectionString: process.env.DATABASE_URL}
+);
 
 pool.on("connect", () => {
-    console.log('Connected to PSQL Database!')
-})
+  console.log("Connected to PSQL Database!");
+});
